49a633c3e8a32f73acfcf17791b29a3ffb43407a
Julian Bieber <j.bieber@patagona.de>
2019-11-15 16:01:12 +0100

    fmt

--- src/api/api.rs
+++ src/api/api.rs
@@ -1,13 +1,13 @@
-use crate::api::message::{MessagesResponse, PostResponse, Message, MessageEvent};
-use crate::api::narrow::Narrow;
 use crate::api::config::ZulipConfig;
+use crate::api::message::{Message, MessageEvent, MessagesResponse, PostResponse};
+use crate::api::narrow::Narrow;
 
-use serde::{Deserialize, Serialize};
-use failure::Error;
-use reqwest::{Client};
 use crate::api::errors::ZulipApiError;
-use crate::api::queue_api::Queue;
 use crate::api::errors::ZulipApiError::ZulipError;
+use crate::api::queue_api::Queue;
+use failure::Error;
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 #[derive(Debug)]
@@ -39,118 +39,167 @@ impl API {
                 ("num_before", format!("{}", num_before).as_str()),
                 ("num_after", format!("{}", num_after).as_str()),
                 (anchor_parameter.0.as_str(), anchor_parameter.1.as_str()),
-                ("narrow", serde_json::to_string(narrows)?.as_str())
+                ("narrow", serde_json::to_string(narrows)?.as_str()),
             ])
             .basic_auth(self.user.as_str(), Some(self.pass.as_str()))
             .send()?
             .json()?;
 
         if response.result == "success" {
-            Ok(MessagesResponse{
+            Ok(MessagesResponse {
                 messages: response.messages.unwrap_or_default(),
                 anchor: response.anchor,
                 found_anchor: response.found_anchor,
                 found_newest: response.found_newest,
-                found_oldest: response.found_oldest
+                found_oldest: response.found_oldest,
             })
         } else {
-            Err(Error::from(ZulipApiError::ZulipError {message: response.msg}))
+            Err(Error::from(ZulipApiError::ZulipError {
+                message: response.msg,
+            }))
         }
     }
 
-    pub fn post_message(&self, stream: &str, topic: &str, message: &str) -> Result<PostResponse, Error> {
+    pub fn post_message(
+        &self,
+        stream: &str,
+        topic: &str,
+        message: &str,
+    ) -> Result<PostResponse, Error> {
         let url = self.build_url("api/v1/messages");
-        let response: InternalPostResponse = self.client
+        let response: InternalPostResponse = self
+            .client
             .post(url.as_str())
             .header("Content-Type", "application/x-www-form-urlencoded")
             .basic_auth(self.user.as_str(), Some(self.pass.as_str()))
-            .body([
-                format!("type=stream"),
-                format!("to={}", stream),
-                format!("subject={}", topic),
-                format!("content={}", message),
-            ].join("&"))
+            .body(
+                [
+                    format!("type=stream"),
+                    format!("to={}", stream),
+                    format!("subject={}", topic),
+                    format!("content={}", message),
+                ]
+                .join("&"),
+            )
             .send()?
             .json()?;
 
         if response.result == "success" {
-            Ok(PostResponse{ id: response.id})
+            Ok(PostResponse { id: response.id })
         } else {
-            Err(Error::from(ZulipApiError::ZulipError {message: response.msg}))
+            Err(Error::from(ZulipApiError::ZulipError {
+                message: response.msg,
+            }))
         }
     }
 
     pub fn mute(&self, stream: &str, topic: &str) -> Result<(), Error> {
-        let response : InternalMuteResponse= self.client.patch(self.build_url("api/v1/users/me/subscriptions/muted_topics").as_str())
+        let response: InternalMuteResponse = self
+            .client
+            .patch(
+                self.build_url("api/v1/users/me/subscriptions/muted_topics")
+                    .as_str(),
+            )
             .basic_auth(self.user.as_str(), Some(self.pass.as_str()))
-            .body([
-                format!("stream={}", stream),
-                format!("topic={}", topic),
-                "op=add".to_string()
-            ].join("&")).send()?
+            .body(
+                [
+                    format!("stream={}", stream),
+                    format!("topic={}", topic),
+                    "op=add".to_string(),
+                ]
+                .join("&"),
+            )
+            .send()?
             .json()?;
         if response.result == "success" {
             Ok(())
         } else {
-            Err(Error::from(ZulipApiError::ZulipError {message: response.msg}))
+            Err(Error::from(ZulipApiError::ZulipError {
+                message: response.msg,
+            }))
         }
     }
 
-    pub fn create_queue(&self, all_public_streams: bool, narrows: &[Narrow]) -> Result<Queue, Error> {
+    pub fn create_queue(
+        &self,
+        all_public_streams: bool,
+        narrows: &[Narrow],
+    ) -> Result<Queue, Error> {
         let url = self.build_url("api/v1/register");
-        let response: InternalRegisterQueueResponse = self.client
-            .post(url.as_str()).header("Content-Type", "application/x-www-form-urlencoded")
+        let response: InternalRegisterQueueResponse = self
+            .client
+            .post(url.as_str())
+            .header("Content-Type", "application/x-www-form-urlencoded")
             .basic_auth(self.user.as_str(), Some(self.pass.as_str()))
-            .body([
-                "event_types=[\"message\"]".to_string(),
-                if all_public_streams {
-                    "all_public_streams=true".to_string()
-                } else {
-                    "all_public_streams=false".to_string()
-                },
-                serde_json::to_string(narrows)?
-            ].join("&")).send()?.json()?;
+            .body(
+                [
+                    "event_types=[\"message\"]".to_string(),
+                    if all_public_streams {
+                        "all_public_streams=true".to_string()
+                    } else {
+                        "all_public_streams=false".to_string()
+                    },
+                    serde_json::to_string(narrows)?,
+                ]
+                .join("&"),
+            )
+            .send()?
+            .json()?;
 
         if response.result == "success" {
-            Ok(Queue{
+            Ok(Queue {
                 id: response.queue_id,
-                last_event_id: response.last_event_id
+                last_event_id: response.last_event_id,
             })
         } else {
-            Err(Error::from(ZulipApiError::ZulipError {message: response.msg}))
+            Err(Error::from(ZulipApiError::ZulipError {
+                message: response.msg,
+            }))
         }
     }
 
     pub fn get_queued_messages(&self, queue: &mut Queue) -> Result<Vec<Message>, Error> {
         let url = self.build_url("api/v1/events");
 
-        let response: MessageQueueResponse = self.client
+        let response: MessageQueueResponse = self
+            .client
             .get(url.as_str())
             .basic_auth(self.user.as_str(), Some(self.pass.as_str()))
             .query(&[
                 ("queue_id", queue.id.as_str()),
-                ("last_event_id", format!("{}", queue.last_event_id).as_str())
-            ]).send()?
+                ("last_event_id", format!("{}", queue.last_event_id).as_str()),
+            ])
+            .send()?
             .json()?;
 
-        let highest = response.events.iter().map(|e| e.id).max().unwrap_or(queue.last_event_id);
+        let highest = response
+            .events
+            .iter()
+            .map(|e| e.id)
+            .max()
+            .unwrap_or(queue.last_event_id);
         queue.last_event_id = highest;
         if response.result == "success" {
             Ok(response.events.into_iter().map(|e| e.message).collect())
         } else {
-            Err(Error::from(ZulipApiError::ZulipError {message: response.msg}))
+            Err(Error::from(ZulipApiError::ZulipError {
+                message: response.msg,
+            }))
         }
     }
 
     pub fn create_stream(&self, name: &str, announce: bool) -> Result<i64, Error> {
         let url = self.build_url("/api/v1/users/me/subscriptions");
 
-
         let mut form = HashMap::new();
-        form.insert("subscriptions", format!("[{{\"name\":\"{}\",\"description\":\"\"}}]", name));
+        form.insert(
+            "subscriptions",
+            format!("[{{\"name\":\"{}\",\"description\":\"\"}}]", name),
+        );
         form.insert("announce", format!("{}", announce));
 
-        let response: InternalCreateStreamResponse = self.client
+        let response: InternalCreateStreamResponse = self
+            .client
             .post(url.as_str())
             .basic_auth(self.user.as_str(), Some(self.pass.as_str()))
             .form(&form)
@@ -161,34 +210,48 @@ impl API {
             let stream_id = self.get_stream_id(name)?;
             Ok(stream_id)
         } else {
-            Err(Error::from(ZulipApiError::ZulipError {message: response.msg}))
+            Err(Error::from(ZulipApiError::ZulipError {
+                message: response.msg,
+            }))
         }
-
     }
 
     pub fn get_stream_id(&self, stream: &str) -> Result<i64, Error> {
         let url = self.build_url("/api/v1/get_stream_id");
 
-        let response: InternalGetStreamIdResponse = self.client.get(url.as_str()).basic_auth(self.user.as_str(), Some(self.pass.as_str())).query(&[
-            ("stream", stream)
-        ]).send()?.json()?;
+        let response: InternalGetStreamIdResponse = self
+            .client
+            .get(url.as_str())
+            .basic_auth(self.user.as_str(), Some(self.pass.as_str()))
+            .query(&[("stream", stream)])
+            .send()?
+            .json()?;
 
         if response.result == "success" && response.stream_id.is_some() {
             Ok(response.stream_id.unwrap())
         } else {
-            Err(Error::from(ZulipApiError::ZulipError {message: response.msg}))
+            Err(Error::from(ZulipApiError::ZulipError {
+                message: response.msg,
+            }))
         }
     }
 
     pub fn delete_stream(&self, stream_id: i64) -> Result<(), Error> {
         let url = format!("{}/{}", self.build_url("/api/v1/streams/"), stream_id);
 
-        let response: InternalDeleteStreamResponse = self.client.delete(url.as_str()).basic_auth(self.user.as_str(), Some(self.pass.as_str())).send()?.json()?;
+        let response: InternalDeleteStreamResponse = self
+            .client
+            .delete(url.as_str())
+            .basic_auth(self.user.as_str(), Some(self.pass.as_str()))
+            .send()?
+            .json()?;
 
         if response.result == "success" {
             Ok(())
         } else {
-            Err(Error::from(ZulipError {message: response.msg}))
+            Err(Error::from(ZulipError {
+                message: response.msg,
+            }))
         }
     }
 
@@ -202,7 +265,11 @@ impl API {
     }
 
     pub fn from_config(config: &ZulipConfig) -> API {
-        API::new(config.domain.clone(), config.user.clone(), config.password.clone())
+        API::new(
+            config.domain.clone(),
+            config.user.clone(),
+            config.password.clone(),
+        )
     }
 
     fn build_url(&self, path: &str) -> String {
@@ -218,13 +285,13 @@ impl API {
 struct InternalPostResponse {
     id: i64,
     msg: String,
-    result: String
+    result: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct InternalMuteResponse {
     msg: String,
-    result: String
+    result: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -232,14 +299,14 @@ struct InternalRegisterQueueResponse {
     last_event_id: i64,
     msg: String,
     queue_id: String,
-    result: String
+    result: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct MessageQueueResponse {
     result: String,
     msg: String,
-    events: Vec<MessageEvent>
+    events: Vec<MessageEvent>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -256,18 +323,18 @@ struct InternalMessagesResponse {
 #[derive(Debug, Serialize, Deserialize)]
 struct InternalCreateStreamResponse {
     result: String,
-    msg: String
+    msg: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct InternalGetStreamIdResponse {
     result: String,
     msg: String,
-    stream_id: Option<i64>
+    stream_id: Option<i64>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct InternalDeleteStreamResponse {
     result: String,
-    msg: String
+    msg: String,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -11,32 +11,58 @@ mod tests {
 
     #[test]
     fn test_post_and_get_messages() {
-        let api = API::from_config(&ZulipConfig::from_file(ZULIP_CONFIG_PATH.to_path_buf()).expect("Failed to read config"));
+        let api = API::from_config(
+            &ZulipConfig::from_file(ZULIP_CONFIG_PATH.to_path_buf())
+                .expect("Failed to read config"),
+        );
 
-        let posted = api.post_message(STREAM, TOPIC, "test_post_and_get_messages").expect("Failed to post messages");
+        let posted = api
+            .post_message(STREAM, TOPIC, "test_post_and_get_messages")
+            .expect("Failed to post messages");
 
-        let messages = api.get_messages(1, 1, Some(posted.id), &[
-            Narrow::stream("julian bot test".to_string(), false)
-        ]).expect("Failed to request messages");
+        let messages = api
+            .get_messages(
+                1,
+                1,
+                Some(posted.id),
+                &[Narrow::stream("julian bot test".to_string(), false)],
+            )
+            .expect("Failed to request messages");
 
-        assert!(messages.messages.len() >= 1, "Must receive at least the posted message");
+        assert!(
+            messages.messages.len() >= 1,
+            "Must receive at least the posted message"
+        );
     }
 
     #[test]
     fn subscribe_to_messages() {
         let message = "this message should appear in a queue";
-        let api = API::from_config(&ZulipConfig::from_file(ZULIP_CONFIG_PATH.to_path_buf()).expect("Failed to read config"));
+        let api = API::from_config(
+            &ZulipConfig::from_file(ZULIP_CONFIG_PATH.to_path_buf())
+                .expect("Failed to read config"),
+        );
         let mut queue = api.create_queue(true, &[]).expect("Failed to create queue");
-        api.post_message(STREAM, TOPIC, message).expect("Failed to post message");
-        let messages = api.get_queued_messages(&mut queue).expect("Failed to retrieve queued messages");
-        assert!(messages.len() >= 1, "Must receive at least the posted message");
+        api.post_message(STREAM, TOPIC, message)
+            .expect("Failed to post message");
+        let messages = api
+            .get_queued_messages(&mut queue)
+            .expect("Failed to retrieve queued messages");
+        assert!(
+            messages.len() >= 1,
+            "Must receive at least the posted message"
+        );
         assert_eq!(messages.last().unwrap().content, message);
     }
 
     #[test]
     fn work_with_streams() {
-        let api = API::from_config(&ZulipConfig::from_file(ZULIP_CONFIG_PATH.to_path_buf()).expect("Failed to read config"));
+        let api = API::from_config(
+            &ZulipConfig::from_file(ZULIP_CONFIG_PATH.to_path_buf())
+                .expect("Failed to read config"),
+        );
 
-        api.create_stream("test zulip-rs stream", false).expect("Failed to create stream");
+        api.create_stream("test zulip-rs stream", false)
+            .expect("Failed to create stream");
     }
 }
